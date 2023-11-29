86cac3439bca46d2c93d325432227c718246805d
Daniel Hahn <danhahn1996@gmail.com>
2023-11-29 02:39:45 +0000

    Completed HTML and CSS for homepage - need to adjust to adapt to mobile screens. #1

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function getValorantAgents() {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://valorant-api.com/v1/agents/');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    console.log(xhr.status);
+    console.log(xhr.response);
+  });
+  xhr.send();
+}
+
+// AGENTS API: https://valorant-api.com/v1/agents
+// WEAPONS API: https://valorant-api.com/v1/weapons
+// MAPS API: https://valorant-api.com/v1/maps

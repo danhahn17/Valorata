572452705fc5861b92cf539bf7077b5ba2fabc6e
Daniel Hahn <danhahn1996@gmail.com>
2023-12-06 11:37:29 +0000

    Updated css and started js. #1

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
-function getValorantAgents() {
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://valorant-api.com/v1/agents');
-  xhr.responseType = 'json';
-  xhr.addEventListener('load', function () {
-    console.log(xhr.status);
-    console.log(xhr.response);
-  });
-  xhr.send();
-}
+const $ = document.querySelector();
 
-getValorantAgents();
+// function getValorantAgents() {
+//   const xhr = new XMLHttpRequest();
+//   xhr.open('GET', 'https://valorant-api.com/v1/agents');
+//   xhr.responseType = 'json';
+//   xhr.addEventListener('load', function () {
+//     console.log(xhr.status);
+//     console.log(xhr.response);
+//   });
+//   xhr.send();
+// }
+
+// getValorantAgents();
 
 // AGENTS API: https://valorant-api.com/v1/agents
 // WEAPONS API: https://valorant-api.com/v1/weapons

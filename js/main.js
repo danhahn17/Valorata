b42063be1abf407b880d13e6281c56b831241d7e
Daniel Hahn <danhahn1996@gmail.com>
2023-12-08 10:14:27 +0000

    Completed API updates for agents, weapons and maps - also have placeholders added for selected item pages. #2

--- js/main.js
+++ js/main.js
@@ -4,7 +4,9 @@ function viewAgent() {
   agent.responseType = 'json';
   agent.addEventListener('load', function () {
     for (let i = 0; i < 6; i++) {
-      $agentRow1.appendChild(createAgentElement(agent.response.data[i]));
+      if (i !== 0) {
+        $agentRow1.appendChild(createAgentElement(agent.response.data[i]));
+      }
     }
     for (let i = 6; i < 13; i++) {
       if (i !== 9) {
@@ -21,7 +23,6 @@ function viewAgent() {
   });
   agent.send();
 }
-
 const $agentRow1 = document.querySelector('.agent-row-1');
 const $agentRow2 = document.querySelector('.agent-row-2');
 const $agentRow3 = document.querySelector('.agent-row-3');
@@ -29,16 +30,16 @@ const $agentRow4 = document.querySelector('.agent-row-4');
 
 const $agentUnknown = document.createElement('a');
 $agentUnknown.setAttribute('class', 'agent-select');
-const $triangle = document.createElement('div');
-$triangle.setAttribute('class', 'triangle');
-const $rectangle = document.createElement('div');
-$rectangle.setAttribute('class', 'rectangle');
+const $triangle1 = document.createElement('div');
+$triangle1.setAttribute('class', 'triangle');
+const $rectangle1 = document.createElement('div');
+$rectangle1.setAttribute('class', 'rectangle');
 const $agentName = document.createElement('div');
 $agentName.setAttribute('class', 'agent-name');
 $agentName.textContent = 'TBA';
 
-$agentUnknown.appendChild($triangle);
-$agentUnknown.appendChild($rectangle);
+$agentUnknown.appendChild($triangle1);
+$agentUnknown.appendChild($rectangle1);
 $agentUnknown.appendChild($agentName);
 
 function createAgentElement(index) {
@@ -67,13 +68,38 @@ function createAgentElement(index) {
   return $agentParent;
 }
 
+viewAgent();
+
+const $brimstoneParent = document.createElement('a');
+$brimstoneParent.setAttribute('class', 'agent-select-brimstone');
+const $triangle2 = document.createElement('div');
+$triangle2.setAttribute('class', 'triangle');
+const $rectangle2 = document.createElement('div');
+$rectangle2.setAttribute('class', 'rectangle');
+const $brimstoneImage = document.createElement('img');
+$brimstoneImage.setAttribute(
+  'src',
+  'https://media.valorant-api.com/agents/9f0d8ba9-4140-b941-57d3-a7ad57c6b417/fullportrait.png',
+);
+$brimstoneImage.setAttribute('class', 'agent-picture');
+const $brimstoneName = document.createElement('div');
+$brimstoneName.setAttribute('class', 'agent-name');
+$brimstoneName.textContent = 'Brimstone';
+$brimstoneParent.appendChild($triangle2);
+$brimstoneParent.appendChild($rectangle2);
+$brimstoneParent.appendChild($brimstoneImage);
+$brimstoneParent.appendChild($brimstoneName);
+$agentRow1.prepend($brimstoneParent);
+
 function viewWeapon() {
   const weapon = new XMLHttpRequest();
   weapon.open('GET', 'https://valorant-api.com/v1/weapons/');
   weapon.responseType = 'json';
   weapon.addEventListener('load', function () {
     for (let i = 0; i < 4; i++) {
-      $weaponRow1.appendChild(createWeaponElement(weapon.response.data[i]));
+      if (i !== 0) {
+        $weaponRow1.appendChild(createWeaponElement(weapon.response.data[i]));
+      }
     }
     for (let i = 4; i < 8; i++) {
       $weaponRow2.appendChild(createWeaponElement(weapon.response.data[i]));
@@ -119,23 +145,22 @@ function createWeaponElement(index) {
   return $weaponParent;
 }
 
-// function createCustomWeaponElement(index) {
-//   const $weaponParent = document.createElement('a');
-//   $weaponParent.setAttribute('class', 'weapon-container');
+viewWeapon();
 
-//   const $weaponImage = document.createElement('img');
-//   $weaponImage.setAttribute('src', index.displayIcon);
-//   $weaponImage.setAttribute('class', 'weapon-image-smaller-width');
-
-//   const $weaponName = document.createElement('div');
-//   $weaponName.setAttribute('class', 'weapon-name');
-//   $weaponName.textContent = index.displayName;
-
-//   $weaponParent.appendChild($weaponImage);
-//   $weaponParent.appendChild($weaponName);
-
-//   return $weaponParent;
-// }
+const $odinParent = document.createElement('a');
+$odinParent.setAttribute('class', 'weapon-container-odin');
+const $odinImage = document.createElement('img');
+$odinImage.setAttribute(
+  'src',
+  'https://media.valorant-api.com/weapons/63e6c2b6-4a8e-869c-3d4c-e38355226584/displayicon.png',
+);
+$odinImage.setAttribute('class', 'weapon-image');
+const $odinName = document.createElement('div');
+$odinName.setAttribute('class', 'weapon-name');
+$odinName.textContent = 'Odin';
+$odinParent.appendChild($odinImage);
+$odinParent.appendChild($odinName);
+$weaponRow1.prepend($odinParent);
 
 function viewMap() {
   const map = new XMLHttpRequest();
@@ -143,7 +168,9 @@ function viewMap() {
   map.responseType = 'json';
   map.addEventListener('load', function () {
     for (let i = 0; i < 5; i++) {
-      $mapContainer1.appendChild(createMapElement(map.response.data[i]));
+      if (i !== 0) {
+        $mapContainer1.appendChild(createMapElement(map.response.data[i]));
+      }
     }
     for (let i = 5; i < 10; i++) {
       $mapContainer2.appendChild(createMapElement(map.response.data[i]));
@@ -176,6 +203,26 @@ function createMapElement(index) {
 
   return $mapParent;
 }
+
+viewMap();
+
+const $ascentParent = document.createElement('a');
+$ascentParent.setAttribute('class', 'maps-group-ascent');
+const $ascentImageContainer = document.createElement('div');
+$ascentImageContainer.setAttribute('class', 'map-image-name-container');
+const $ascentImage = document.createElement('img');
+$ascentImage.setAttribute(
+  'src',
+  'https://media.valorant-api.com/maps/7eaecc1b-4337-bbf6-6ab9-04b8f06b3319/listviewicon.png',
+);
+$ascentImage.setAttribute('class', 'map-image');
+const $ascentName = document.createElement('div');
+$ascentName.setAttribute('class', 'map-name');
+$ascentName.textContent = 'Ascent';
+$ascentParent.appendChild($ascentImageContainer);
+$ascentImageContainer.appendChild($ascentImage);
+$ascentImageContainer.appendChild($ascentName);
+$mapContainer1.prepend($ascentParent);
 
 const $homepageTab = document.querySelector('[data-view=homepage]');
 const $agentViewPage1 = document.querySelector(
@@ -400,6 +447,18 @@ const $clickAgentTab = document.querySelector('.agents-tab');
 const $clickNextAgent = document.querySelector('.next-agent-button');
 const $clickPreviousAgent = document.querySelector('.previous-agent-button');
 
+const $clickWeaponsTab = document.querySelector('.weapons-tab');
+const $clickNextWeapon = document.querySelector('.next-weapon-button');
+const $clickPreviousWeapon = document.querySelector('.previous-weapon-button');
+
+const $clickMapTab = document.querySelector('.maps-tab');
+const $clickNextMap = document.querySelector('.next-maps-button');
+const $clickPreviousMap = document.querySelector('.previous-maps-button');
+
+const $clickCreateCard = document.querySelector('.create-a-card-tab');
+const $clickSave = document.querySelector('.create-card-save-button');
+const $clickGallery = document.querySelector('.gallery-tab');
+
 $clickedLogo.addEventListener('click', function (event) {
   viewSwap('homepage');
 });
@@ -407,7 +466,6 @@ $clickedLogo.addEventListener('click', function (event) {
 $clickAgentTab.addEventListener('click', function (event) {
   viewSwap('agents-view-page-1');
 });
-$clickAgentTab.addEventListener('click', viewAgent());
 
 $clickNextAgent.addEventListener('click', function (event) {
   viewSwap('agents-view-page-2');
@@ -417,79 +475,54 @@ $clickPreviousAgent.addEventListener('click', function (event) {
   viewSwap('agents-view-page-1');
 });
 
-// $selectedAgentPage
-// $weaponViewPage1
-// $weaponViewPage2
-// $selectedWeaponPage
-// $mapViewPage1
-// $mapViewPage2
-// $selectedMapPage
-// $createCardPage
-// $galleryPage
-// $selectedCardPage
+const $clickAgent = document.querySelector('.agent-select-brimstone');
+$clickAgent.addEventListener('click', function (event) {
+  viewSwap('selected-agent-page');
+});
 
-// const $clickAgent = document.querySelector('.agent-select');
-// $clickAgent.addEventListener('click', function (event) {
-//   viewSwap('selected-agent-page');
-// });
-
-const $clickWeaponsTab = document.querySelector('.weapons-tab');
 $clickWeaponsTab.addEventListener('click', function (event) {
   viewSwap('weapons-view-page-1');
 });
-$clickWeaponsTab.addEventListener('click', viewWeapon());
 
-const $clickNextWeapon = document.querySelector('.next-weapon-button');
 $clickNextWeapon.addEventListener('click', function (event) {
   viewSwap('weapons-view-page-2');
 });
 
-const $clickPreviousWeapon = document.querySelector('.previous-weapon-button');
 $clickPreviousWeapon.addEventListener('click', function (event) {
   viewSwap('weapons-view-page-1');
 });
 
-// const $clickWeapon1 = document.querySelector('.weapon-container');
-// const $clickWeapon2 = document.querySelector('.weapon-container-last');
-// $clickWeapon1.addEventListener('click', function (event) {
-//   viewSwap('selected-weapon-page');
-// });
-// $clickWeapon2.addEventListener('click', function (event) {
-//   viewSwap('selected-weapon-page');
-// });
+const $clickWeapon1 = document.querySelector('.weapon-container-odin');
+$clickWeapon1.addEventListener('click', function (event) {
+  viewSwap('selected-weapon-page');
+});
 
-const $clickMapTab = document.querySelector('.maps-tab');
 $clickMapTab.addEventListener('click', function (event) {
   viewSwap('maps-view-page-1');
 });
 $clickMapTab.addEventListener('click', viewMap());
 
-const $clickNextMap = document.querySelector('.next-maps-button');
 $clickNextMap.addEventListener('click', function (event) {
   viewSwap('maps-view-page-2');
 });
 
-const $clickPreviousMap = document.querySelector('.previous-maps-button');
 $clickPreviousMap.addEventListener('click', function (event) {
   viewSwap('maps-view-page-1');
 });
 
-// const $clickMap = document.querySelector('.maps-group');
-// $clickMap.addEventListener('click', function (event) {
-//   viewSwap('selected-map-page');
-// });
+const $clickMap = document.querySelector('.maps-group-ascent');
+$clickMap.addEventListener('click', function (event) {
+  viewSwap('selected-map-page');
+});
 
-const $clickCreateCard = document.querySelector('.create-a-card-tab');
 $clickCreateCard.addEventListener('click', function (event) {
   viewSwap('create-a-card-page');
 });
 
-const $clickSave = document.querySelector('.create-card-save-button');
 $clickSave.addEventListener('click', function (event) {
   viewSwap('gallery-page');
 });
 
-const $clickGallery = document.querySelector('.gallery-tab');
 $clickGallery.addEventListener('click', function (event) {
   viewSwap('gallery-page');
 });
